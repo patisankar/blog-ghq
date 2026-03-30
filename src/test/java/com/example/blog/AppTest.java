69b697ac9365532b9033d5633a175667f31f51b8
patisankar <patisankarjava@gmail.com>
2026-03-30 18:42:19 -0500

    Create AppTest.java

---
+++ src/test/java/com/example/blog/AppTest.java
@@ -0,0 +1,16 @@
+package com.example.blog;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    @Test
+    void testApp() {
+        assertTrue(true);
+    }
+}
